24047265d9eefac28fde29acc55183ab35ca391b
WU JIANGANG <e1504042@u.nus.edu>
2026-06-27 17:12:08 +0800

    note: mianshiya python 20260626

--- mianshiya/Python/面试鸭python20260616/test.py
+++ mianshiya/Python/面试鸭python20260616/test.py
@@ -1,3 +1,4 @@
+"""
 a = 0b1100
 print(a)
 b = ~a
@@ -7,3 +8,19 @@ a = -0b1100
 print(a)
 b = ~a
 print(b)
+
+
+"""
+import math
+
+print(math.pi)
+print(math.cos(math.pi))
+print(math.sin(math.pi))
+print(math.sqrt(4))
+print(abs(-4))
+
+a = [1,5,9,10,80,2,3,5,9,6]
+a_vi = [(a[i],i) for i in range(len(a))]
+print(a_vi)
+a_vi.sort(key=lambda x: x[0],reverse=True)
+print(a_vi)
